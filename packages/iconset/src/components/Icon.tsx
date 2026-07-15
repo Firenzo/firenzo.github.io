2fd81628752256dd9cc6b3a53d4b7f3517e508f6
Firenzo Jorden <firenzo.jorden@gmail.com>
2026-07-15 03:41:47 +0200

    refactor icon component

--- packages/iconset/src/components/Icon.tsx
+++ packages/iconset/src/components/Icon.tsx
@@ -3,13 +3,14 @@ import React from "react";
 // if the import fails run the build script first to generate a .ts file containing this type in dist
 import type { IconClassName, IconSize, IconColor } from "../../dist/getIcon";
 
-export interface IconProps {
+export type IconProps = {
   icon: IconClassName;
   size?: IconSize;
   color?: IconColor;
-}
+};
 
-const Icon: React.FC<IconProps> = ({ icon, size = 24, color = "primary" }) => {
+const Icon = (props: IconProps) => {
+  const { icon, size = 24, color = "primary" } = props;
   const classes = `${icon} icon-size-${size} icon-color-${color}`;
 
   return <i className={classes} aria-hidden="true" />;
