08a5a74cc305088b2abfa69a4ee8db9cfbd6a258
Firenzo Jorden <firenzo.jorden@gmail.com>
2026-07-15 03:43:02 +0200

    add Astro middleware

---
+++ apps/astro/src/middleware/index.ts
@@ -0,0 +1,34 @@
+import type { MiddlewareHandler } from "astro";
+import type { PageData } from "../schemas/pageSchemas";
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  /**
+   * This middleware fetches data of existing pages and
+   * adds the data to the Astro.locals object.
+   */
+  const availablePages = ["/about-me", "/resume", "/creations", "/contact"];
+  const currentPath =
+    context.url.pathname === "/" ? "/creations" : context.url.pathname;
+  if (availablePages.includes(currentPath)) {
+    const pageBaseUrl = "http://localhost:1337/api/page";
+    try {
+      const response = await fetch(`${pageBaseUrl}${currentPath}`);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const pageData = await response.json();
+      if (context.locals) {
+        context.locals.pageData = pageData as PageData;
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        throw err;
+      }
+      throw new Error(String(err));
+    }
+  }
+
+  return next();
+};
