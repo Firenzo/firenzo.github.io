6c906a9461516d21b1bd8fa24b0d16557e519de3
Firenzo Jorden <firenzo.jorden@gmail.com>
2026-08-02 17:39:45 +0200

    decrease CLS by importing fonts with astro provider and setting pixel value on container max width

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -8,6 +8,21 @@ export default defineConfig({
   redirects: {
     "/admin": "http://localhost:1337/admin",
   },
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: [900],
+    },
+    {
+      provider: fontProviders.adobe({ id: "xnt8pgt" }),
+      name: "Proxima Nova",
+      cssVariable: "--font-proxima-nova",
+      weights: [300, 400, 700],
+    },
+  ],
 
   integrations: [react()],
 });
